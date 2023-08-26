53cbcae762982bb22e4c98505073cc860fe39f17
Fallou23 <fallou23j@gmail.com>
2023-08-26 16:54:09 +0200

    debug

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9nor@w)_1#3d!p2-sph+9zx^z2@=+4mdp$x-de#(n5#jy=de9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['uniaway-61bad5bc656e.herokuapp.com','127.0.0.1']
 
