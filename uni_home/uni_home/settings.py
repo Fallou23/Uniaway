00760ef9ed8eef593d1ba4a9f1fbd41363eb633c
Fallou23 <fallou23j@gmail.com>
2023-08-29 19:41:33 +0200

    db

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'uni_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Mynigga99',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
