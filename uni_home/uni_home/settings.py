22f12cc6610bb99e31a2fd9f4a213b60f7d4a508
Fallou23 <fallou23j@gmail.com>
2023-08-29 20:49:23 +0200

    sett

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9nor@w)_1#3d!p2-sph+9zx^z2@=+4mdp$x-de#(n5#jy=de9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','uniaway-61bad5bc656e.herokuapp.com','127.0.0.1','www.uniaway.it','uniaway.it']
 
@@ -164,7 +164,7 @@ EMAIL_USE_SSL = False
 SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
 
 
-SECURE_SSL_REDIRECT=False
+SECURE_SSL_REDIRECT=True
 
 
 
