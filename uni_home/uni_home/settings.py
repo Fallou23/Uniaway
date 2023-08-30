d251c5d3e78e1dfa57b2ab56a16a034eeb1d756c
Fallou23 <fallou23j@gmail.com>
2023-08-30 20:34:18 +0200

    o

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -156,6 +156,8 @@ AWS_STORAGE_BUCKET_NAME = 'uniaway'
 AWS_S3_REGION_NAME = 'eu-west-3'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
+AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
