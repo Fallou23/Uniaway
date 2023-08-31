50adadb7c8c91bf8a71a2c7a9f918a589920a505
Fallou23 <fallou23j@gmail.com>
2023-08-31 21:28:34 +0200

    static

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -139,7 +139,7 @@ else:
     
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
