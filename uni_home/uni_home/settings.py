c393155ef3c114645c90519f929d7b49146795c0
Fallou23 <fallou23j@gmail.com>
2023-08-31 20:02:10 +0200

    ff

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,7 +48,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    #'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,6 +136,8 @@ if DEBUG:
         )
 else:
     STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+    
+django_heroku.settings(locals())
 
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
