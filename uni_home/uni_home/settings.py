98f4abbcb86d25af71c0c4c3f3a37792be3cdcbb
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2023-09-15 14:54:35 +0530

    Update settings.py

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9nor@w)_1#3d!p2-sph+9zx^z2@=+4mdp$x-de#(n5#jy=de9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
