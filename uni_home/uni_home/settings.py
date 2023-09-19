1c107348a33d9bfb763797bdd69d688bfd90a8ae
Fallou23 <fallou23j@gmail.com>
2023-09-19 19:58:57 +0200

    aws

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -198,5 +198,6 @@ AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
