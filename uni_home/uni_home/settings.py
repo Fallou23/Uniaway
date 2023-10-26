c31f6aa5735322999de160963195680c13839a26
Fallou23 <fallou23j@gmail.com>
2023-10-26 19:44:07 +0200

    updated

--- uni_home/uni_home/settings.py
+++ uni_home/uni_home/settings.py
@@ -168,8 +168,8 @@ else:
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #MEDIA_URL = 'https://uniaway.s3.amazonaws.com/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATIC_URL = 'https://uniaway.s3.amazonaws.com/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
